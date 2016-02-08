b83cdd2ca06e7f1c1c2fb8fc3ebd0b1bab22878b
Ludovic Viaud <ludovic.viaud@gmail.com>
2016-02-08 00:52:34 -0300

    feat: layout sidebar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,11 +7,11 @@ var Octopus = require('./octopus');
 
 ko.applyBindings(new Octopus(places));
 
-//// Google Map
-//window.map = undefined;
-//window.initMap = function () {
-//    map = new google.maps.Map(document.getElementById('map'), {
-//        center: {lat: -34.397, lng: 150.644},
-//        zoom: 8
-//    });
-//};
+// Google Map
+window.map = undefined;
+window.initMap = function () {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+};
